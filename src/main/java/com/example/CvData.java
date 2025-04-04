d85532b3850a18367419c44fcc05f9b6308ce2bf
chrismag06 <chrismag06000@gmail.com>
2025-04-04 16:49:01 +0200

    add all files

---
+++ src/main/java/com/example/CvData.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.List;
+
+public class CvData {
+    public String name;
+    public String email;
+    public String phone;
+    public List<String> skills;
+    public List<String> experience;
+    public List<String> education;
+}
